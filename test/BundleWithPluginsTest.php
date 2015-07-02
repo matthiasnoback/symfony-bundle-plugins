40f56f5b24aa9a621bba130df2be667b6336cd87
Dennis de Greef <github@link0.net>
2015-07-02 17:54:50 +0200

    Aligned path to test folder
    Rewritten helper method to be more explicit
    
    Let PHPUnit create a coverage report
    
    Override these methods so the container cache won't be used, and every tests runs 'individually'.
    
    Use another workaround for container cache inside Kernel. Using system temporary folders
    
    We don't need this temp directory anymore
    
    Added Travis configuration file
    
    Use uniqid in system temp folder instead of tempnam
    
    Removed unused variable
    
    Removing COMPOSER_ROOT_VERSION, since it is not needed

--- test/BundleWithPluginsTest.php
+++ test/BundleWithPluginsTest.php
@@ -5,18 +5,16 @@ namespace Matthias\BundlePlugins\Tests;
 use Matthias\BundlePlugins\BundlePlugin;
 use Symfony\Component\HttpKernel\Kernel;
 
-class BundleWithPluginsTest extends \PHPUnit_Framework_TestCase
+class BundleWithPluginsTest extends IsolatedKernelTestCase
 {
     /**
      * @test
      */
     public function it_only_loads_configuration_for_enabled_bundles_1()
     {
-        $kernel = new AppKernel(
-            array(),
-            array(new FooPlugin())
-        );
-        $kernel->boot();
+        $kernel = $this->createKernel(array(), array(
+            new FooPlugin(),
+        ));
 
         $this->pluginIsLoaded($kernel, new CorePlugin());
         $this->pluginIsLoaded($kernel, new FooPlugin());
@@ -27,20 +25,25 @@ class BundleWithPluginsTest extends \PHPUnit_Framework_TestCase
      */
     public function it_only_loads_configuration_for_enabled_bundles_2()
     {
-        $kernel = new AppKernel(
-            array(),
-            array(new FooPlugin(), new BarPlugin())
-        );
-        $kernel->boot();
+        $kernel = $this->createKernel(array(), array(
+            new FooPlugin(),
+            new BarPlugin(),
+        ));
 
         $this->pluginIsLoaded($kernel, new CorePlugin());
         $this->pluginIsLoaded($kernel, new FooPlugin());
         $this->pluginIsLoaded($kernel, new BarPlugin());
     }
 
-    private function pluginIsLoaded(Kernel $httpKernel, BundlePlugin $plugin)
+    /**
+     * @param Kernel $httpKernel
+     * @param string $plugin
+     */
+    private function pluginIsLoaded(Kernel $httpKernel, $plugin)
     {
-        $this->assertTrue($httpKernel->getContainer()->hasParameter($plugin->name() . '.loaded'));
+        $parameterName = $plugin->name() . '.loaded';
+        $hasParameter = $httpKernel->getContainer()->hasParameter($parameterName);
+        $this->assertTrue($hasParameter, "Failed asserting DI Container has parameter '" . $parameterName . "'");
         $this->assertTrue($plugin::$built);
         $this->assertTrue($plugin::$booted);
     }
